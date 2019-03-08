9d56ebf730dee4cbdf777599b234adf39b04d8b4
Liz James <rjames93@sucs.org>
2019-03-08 12:06:43 +0000

    bangs head on desk
     Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import pymesh
 import pyrender
 import numpy as np
 import trimesh
-import moderngl
 import random
 import math
-import pyglet
-from pyglet.gl import *
+import pygame
+import OpenGL
+
+
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 points = [] # Simple 2D for initial testing
-n_points = 100000
+n_points = 10000
 
 
 for i in range(n_points):
@@ -28,17 +32,42 @@ for i in range(n_points):
 # Now just to check we have the target number of unique points
 
 nppoints = np.asarray(points)
-
+print(nppoints)
 meshgen = pymesh.tetgen();
-meshgen.points = points;
-meshgen.verbosity = 1
+meshgen.points = nppoints;
+meshgen.verbosity = 0
 meshgen.run()
 
-mesh = pymesh.subdivide(meshgen.mesh, order = 1, method="loop")
+#mesh = pymesh.subdivide(meshgen.mesh, order = 1, method="loop")
+mesh = meshgen.mesh.normalize()
 
 
+def renderObject():
+    for face in mesh.faces:
+        glBegin(GL_TRIANGLES)
+        glVertex3f(face[0][0],face[0][1],face[0][2])
+        glVertex3f(face[1][0],face[1][1],face[1][2])
+        glVertex3f(face[2][0],face[2][1],face[2][2])
+        glEnd()
 
-print(mesh.faces)
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0,0.0, -5)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+    glRotatef(1, 3, 1, 1)
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    renderObject()
+    pygame.display.flip()
+    pygame.time.wait(10)
+
+main()
 
 pymesh.save_mesh("output.stl", mesh) # For debugging purposes
 tm = trimesh.load('output.stl')
